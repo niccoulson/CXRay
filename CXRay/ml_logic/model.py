a2aa6e424989af7ff604d659b166bd16458cdde4
niccoulson <114218919+niccoulson@users.noreply.github.com>
2023-02-21 13:10:33 +0200

    Included basic model in the backend and started the backend for the interface

--- CXRay/ml_logic/model.py
+++ CXRay/ml_logic/model.py
@@ -2,5 +2,57 @@
 from tensorflow.keras import Model, Sequential, layers, regularizers, optimizers
 from tensorflow.keras.callbacks import EarlyStopping
 import numpy as np
+from typing import Tuple
 
-# Initialize model 
+# Initialize model
+
+def initialize_model(X: np.ndarray) -> Model:
+    """
+    Initialize the Convelutional Neural Network
+    """
+
+    model = Sequential()
+
+    print("\n✅ model initialized")
+
+    return model
+
+
+
+def compile_model(model: Model, learning_rate: float) -> Model:
+    """
+    Compile the Convelutional Neural Network
+    """
+
+    print("\n✅ model compiled")
+
+    return model
+
+def train_model(model: model,
+                X: np.ndarray,
+                y: np.ndarray,
+                batch_size=64,
+                patience=2,
+                validation_split=0.3,
+                validation_data=None) -> Tuple[Model, dict]:
+    """
+    Fit model and return a the tuple (fitted_model, history)
+    """
+
+    es = EarlyStopping(monitor="val_loss",
+                       patience=patience,
+                       restore_best_weights=True,
+                       verbose=0)
+
+    history = model.fit(X,
+                        y,
+                        validation_split=validation_split,
+                        validation_data=validation_data,
+                        epochs=100,
+                        batch_size=batch_size,
+                        callbacks=[es],
+                        verbose=0)
+
+    print(f"\n✅ model trained ({len(X)} rows)")
+
+    return model, history
