d33534b4506b767a3602048b5920011e3d227ec8
niccoulson <114218919+niccoulson@users.noreply.github.com>
2023-02-22 09:55:28 +0200

    Continued progress on backend structure. Currently implementing data retreval

--- CXRay/ml_logic/data.py
+++ CXRay/ml_logic/data.py
@@ -2,3 +2,17 @@
 import os
 import pandas as pd
 import numpy as np
+
+
+if os.environ['DATA_SOURCE'] == 'local':
+    chunk_df = get_xray_local()  # function to get local data
+
+    return chunk_df
+
+elif os.environ['DATA_SOURCE'] == 'cloud':
+    chunk_df = get_xray_cloud() # Function to get cloud data
+
+    return chunk_df
+
+else: 
+    raise ValueError(f'Value in .env {os.environ["DATA_SOURCE"]} is unknown')
